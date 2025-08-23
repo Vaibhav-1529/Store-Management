4b4a724142a63ad8c0776139797190237c0a2d85
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-08-23 09:55:44 +0530

    cors add

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
-      {hostname:"**"}
-    ]
-  }
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
-
